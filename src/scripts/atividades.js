37b2bfb075e12c6e8a4691a8e02f4786c7da755f
Rubens Pereira da Silva <89171885+Rubens-Pereira-Silva@users.noreply.github.com>
2026-03-11 16:12:30 -0300

    botão de avançar e voltar na teoria , lista com os textos da teoria adicionados a classe de atividades

--- src/scripts/atividades.js
+++ src/scripts/atividades.js
@@ -1,8 +1,14 @@
 // constantes com os elementos html
 const sectionAtividades = document.getElementById("atividades");
+
+// consts modal
 const teoriaDialog = document.getElementById("teoriaDialog");
+const teoriaModalParagrafo = document.getElementById("teoriaModalParagrafo");
 
 let atividades = pegarAtividades();
+
+let atividadeAtual;
+let numeroAtividadeAtual = 0;
 
 function verificarNivel(nivelAtt) {
   const jogador = statusJogador();
@@ -11,12 +17,14 @@ function verificarNivel(nivelAtt) {
   }
   return false;
 }
-
+//Botões de abrir os models
 function btnteoria(nivelAtt) {
   if (verificarNivel(nivelAtt)) {
     console.log("Botão de teoria precionado");
+    atividadeAtual = atividades[nivelAtt];
     teoriaDialog.showModal();
     document.body.style.overflow = "hidden";
+    attModal();
   }
 }
 function btnpratica(nivelAtt) {
@@ -25,9 +33,24 @@ function btnpratica(nivelAtt) {
   }
 }
 
+//funções do modal
 function btnfecharModal() {
   teoriaDialog.close();
   document.body.style.overflow = "auto";
+}
+
+function attModal() {
+  teoriaModalParagrafo.innerText = atividadeAtual.teoria[numeroAtividadeAtual];
+}
+
+function mudarPaginaTeoria(lado) {
+  if (lado == 1 && numeroAtividadeAtual > 0) {
+    numeroAtividadeAtual -= 1;
+  }
+  if (lado == 2 && numeroAtividadeAtual < atividadeAtual.teoria.length - 1) {
+    numeroAtividadeAtual += 1;
+  }
+  attModal();
 }
 
 function init() {
@@ -35,7 +58,7 @@ function init() {
     const atividade = atividades[i];
 
     sectionAtividades.innerHTML += `
-      <button onClick="btn${atividade.type}(${i + 1})" class="atividade">
+      <button onClick="btn${atividade.type}(${i})" class="atividade">
           <div class="circuloAtividade ${atividade.type}">
             <img src="/src/images/${atividade.icon}" alt="" />
           </div>
--- src/scripts/services/api.js
+++ src/scripts/services/api.js
@@ -1,18 +1,27 @@
 class Atividade {
-  constructor(type, nome, icon) {
+  constructor(type, nome, icon, teoria) {
     this.type = type;
     this.nome = nome;
     this.icon = icon;
+    this.teoria = teoria;
   }
 }
 
+let ListaTeoriaTeste = [
+  "Aqui temos a primeira pagina da teoria",
+  "aqui temos a segunda pagina da teoria",
+  "aqui podemos ver a terceira pagina da teoria",
+  "a quarta tambem esta aqui",
+  "para finalizar a quinta tambem esta entre nos",
+];
+
 function pegarAtividades() {
   return [
-    new Atividade("teoria", "Reciclagem", "livro.png"),
-    new Atividade("teoria", "Reciclagem", "livro.png"),
-    new Atividade("teoria", "Reciclagem", "livro.png"),
-    new Atividade("teoria", "Reciclagem", "livro.png"),
-    new Atividade("pratica", "Reciclagem", "haltere.png"),
+    new Atividade("teoria", "Reciclagem", "livro.png", ListaTeoriaTeste),
+    new Atividade("teoria", "Reciclagem", "livro.png", ListaTeoriaTeste),
+    new Atividade("teoria", "Reciclagem", "livro.png", ListaTeoriaTeste),
+    new Atividade("teoria", "Reciclagem", "livro.png", ListaTeoriaTeste),
+    new Atividade("pratica", "Reciclagem", "haltere.png", ""),
   ];
 }
 
