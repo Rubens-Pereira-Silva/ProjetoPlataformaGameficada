191aaacbfd72be6c4597234e2f8dc33a8ddf1ff4
Rubens Pereira da Silva <89171885+Rubens-Pereira-Silva@users.noreply.github.com>
2026-05-06 20:34:57 -0300

    Barra de navegação modificada, paginas extras criadas

--- src/scripts/services/header.js
+++ src/scripts/services/header.js
@@ -1,4 +1,25 @@
-//Funcionamento do modal
+//Header
+
+const header = document.getElementById("header")
+
+header.innerHTML = `
+  <button onclick="" id="btnPerfil">
+        <img src="../images/perfil.png" alt="" />
+      </button>
+      <button onclick="" id="btnLoja">
+        <img src="../images/loja.png" alt="" />
+      </button>
+      <button onclick="" id="btnLeaderboard">
+        <img src="../images/podio.png" alt="" />
+      </button>
+      <button onclick="" id="btnConfig">
+        <img src="../images/configuracao.svg" alt="" />
+      </button>
+`
+
+
+
+/*Funcionamento do modal
 const btnConfig = document.getElementById("btnConfig");
 const btnFecharConfig = document.getElementById("btnFecharConfig");
 
@@ -54,3 +75,25 @@ inputListaTemas.addEventListener("change", (event) => {
 
   document.body.className = novoTema;
 });
+*/
+
+//Navegação Perfil
+const btnPerfil = document.getElementById("btnPerfil")
+
+btnPerfil.onclick = () => {
+  window.location.replace("perfil.html")  
+}
+
+//Navegação Leaderboard
+const btnLeaderboard = document.getElementById("btnLeaderboard")
+
+btnLeaderboard.onclick = () => {
+  window.location.replace("leaderboard.html")
+}
+
+//Navegação Loja
+const btnLoja = document.getElementById("btnLoja")
+
+btnLoja.onclick = () => {
+  window.location.replace("loja.html")
+}
