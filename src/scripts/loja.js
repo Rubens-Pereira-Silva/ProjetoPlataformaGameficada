0269650f5e5f54f2cf4abfd8cf8f924f13fba517
Rubens Pereira da Silva <89171885+Rubens-Pereira-Silva@users.noreply.github.com>
2026-05-07 14:30:11 -0300

    Usar a loja para trocar o icone ja esta funcionando , apenas colocar verificação de valor e retirada do valor para a "compra" do icon

--- src/scripts/loja.js
+++ src/scripts/loja.js
@@ -1,26 +1,42 @@
 const itemsVenda = [
-    "pirata",
-    "ninja",
-    "astronauta",
-    "pirata",
-    "ninja",
-    "astronauta",
-    "pirata",
-    "ninja",
-    "astronauta",
-    "pirata",
-    "ninja",
-    "astronauta"
-]
+  "pirata",
+  "ninja",
+  "astronauta",
+  "pirata",
+  "ninja",
+  "astronauta",
+  "pirata",
+  "ninja",
+  "astronauta",
+  "pirata",
+  "ninja",
+  "astronauta",
+];
 
-const loja = document.getElementById("loja")
+async function comprarIcon(nomeIcon) {
+  const res = await fetch(
+    "https://projetoplataformaunip-api-1.onrender.com/usuario/loja/icon/" +
+      localStorage.getItem("ID"),
+    {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: nomeIcon.toString(),
+    }
+  );
 
-itemsVenda.forEach(item => {
-    loja.innerHTML += `
+  const data = await res.json();
+  console.log(data);
+}
+
+const loja = document.getElementById("loja");
+
+itemsVenda.forEach((item) => {
+  loja.innerHTML += `
         <div class="itemLoja">
             <img src="../images/iconsJogador/${item}.png" />
-
-            <button>Comprar</button>
+            <button onclick=comprarIcon("${item}")>Comprar</button>
         </div>
-    `
-})
+    `;
+});
